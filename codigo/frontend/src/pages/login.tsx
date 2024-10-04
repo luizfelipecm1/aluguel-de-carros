dc8568ef06e8ca09e9487d16e26216a1d51dc8ac
luizfelipecm1 <142749233+luizfelipecm1@users.noreply.github.com>
2024-10-04 19:28:51 -0300

    feat: login backend

--- codigo/frontend/src/pages/login.tsx
+++ codigo/frontend/src/pages/login.tsx
@@ -1,7 +1,32 @@
+import axios from "axios";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
+import { useEffect, useState } from "react";
 
 export function Login() {
+  const [users, setUsers] = useState<any[]>([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/usuarios')
+        .then(response => {
+          console.log(response.data); // Log data to check fields
+          setUsers(response.data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleLogin = () => {
+    const user = users.find(user => user.email === email && user.password === password);
+    if (user) {
+      console.log("Login successful");
+      // Redirect or perform other actions
+    } else {
+      console.error("Invalid credentials");
+    }
+  };
+
   return (
     <div className="container mx-auto h-screen grid place-content-center text-center gap-4">
       <h1 className="text-3xl font-bold">Login</h1>
@@ -9,15 +34,23 @@ export function Login() {
         <span className="p-inputgroup-addon">
           <i className="pi pi-at"></i>
         </span>
-        <InputText placeholder="E-mail" />
+        <InputText 
+          placeholder="E-mail" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
       </div>
       <div className="p-inputgroup flex-1">
         <span className="p-inputgroup-addon">
           <i className="pi pi-lock"></i>
         </span>
-        <InputText placeholder="Senha" />
+        <InputText 
+          placeholder="Senha" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
       </div>
-      <Button label="Entrar" />
+      <Button label="Entrar" onClick={handleLogin} />
       <Button
         label="Cadastrar"
         link
