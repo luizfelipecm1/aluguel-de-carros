dc2364393f7f7f45646deb6c06b11983dfc41dd2
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2024-10-04 01:19:43 -0300

    feat: login page

--- codigo/frontend/src/pages/login.tsx
+++ codigo/frontend/src/pages/login.tsx
@@ -1,9 +1,23 @@
 import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 
 export function Login() {
   return (
-    <>
-      <Button label="Login" />
-    </>
+    <div className="container mx-auto h-screen grid place-content-center text-center gap-4">
+      <h1 className="text-3xl font-bold">Login</h1>
+      <div className="p-inputgroup flex-1">
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-at"></i>
+        </span>
+        <InputText placeholder="E-mail" />
+      </div>
+      <div className="p-inputgroup flex-1">
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-lock"></i>
+        </span>
+        <InputText placeholder="Senha" />
+      </div>
+      <Button label="Entrar" />
+    </div>
   );
 }
